f49960d8c5f3982ca8e51f3bebb1ac116a2bdafc
GualiTaha <gualitaha@gmail.com>
2021-11-30 23:36:59 +0100

    deploy

---
+++ app/build/precache-manifest.85b39e2c59045eb1755d4facc2ef3c60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a9a61a422a39b327b7899dbf7b2d3a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ddc518b4d389ac9586b",
+    "url": "/static/css/main.92e10481.chunk.css"
+  },
+  {
+    "revision": "81684b00cdf418626f6d",
+    "url": "/static/js/2.1fe6efe2.chunk.js"
+  },
+  {
+    "revision": "3f9558f3982572c5fb29a3aa309d2d82",
+    "url": "/static/js/2.1fe6efe2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ddc518b4d389ac9586b",
+    "url": "/static/js/main.fca645a5.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "0c6815a4de64b534d26db4abd1c804f5",
+    "url": "/static/media/food.0c6815a4.svg"
+  }
+]);
